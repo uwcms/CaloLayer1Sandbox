b98698b70ece4e6183c4fe0822c66edc470b05f6
Tyler Henry Ruggles <truggles@srv-C2C03-22.cms>
2015-08-04 13:00:55 +0200

    added #

--- cactusprojects/CaloLayer1/ts/cell/include/CaloLayer1/ts/cell/version.h
+++ cactusprojects/CaloLayer1/ts/cell/include/CaloLayer1/ts/cell/version.h
@@ -1,8 +1,8 @@
 /*=============================================================================
 Copy of version style from cactus/include/boost/spirit/version.hpp
 =============================================================================*/
-#if !defined(CALOLAYER1_VERSION_AUGUST_04_2015_1150AM)
-#define CALOLAYER1_VERSION_AUGUST_04_2015_1150AM
+#if !defined(CALOLAYER1_VERSION_0_0_1_AUGUST_04_2015_1150AM)
+#define CALOLAYER1_VERSION_0_0_1_AUGUST_04_2015_1150AM
 
 #include <CaloLayer1/ts/cell/include/version.h>
 
