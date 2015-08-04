cff8b9bc27009330a6cfb47050de7e8973fc1c81
Tyler Ruggles <truggles@wisc.edu>
2015-08-04 11:55:08 +0200

    Based off of other 'version' files in cactus proj

---
+++ cactusprojects/CaloLayer1/ts/cell/include/CaloLayer1/ts/cell/version.h
@@ -0,0 +1,9 @@
+/*=============================================================================
+Copy of version style from cactus/include/boost/spirit/version.hpp
+=============================================================================*/
+#if !defined(CALOLAYER1_VERSION_AUGUST_04_2015_1150AM)
+#define CALOLAYER1_VERSION_AUGUST_04_2015_1150AM
+
+#include <CaloLayer1/ts/cell/include/version.h>
+
+#endif
